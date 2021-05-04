68a9f5e5fc11d04a687f38cca114551fee01867d
Sobahan.webhunter <sobahan.webhunter@gmail.com>
2021-05-04 18:30:47 +0600

    after completing testimonia and form part

--- js/script.js
+++ js/script.js
@@ -117,3 +117,87 @@ $('.team-slider').slick({
     // instead of a settings object
   ]
 });
+
+
+$('.testimonial-text-slider').slick({
+  dots: false,
+  infinite: true,
+  
+  speed: 300,
+  autoplay: true,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.testimonial-img-slider',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.testimonial-img-slider').slick({
+  dots: true,
+  infinite: true,
+ 
+  speed: 300,
+  autoplay: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "0",
+  asNavFor: '.testimonial-text-slider',
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
